fde57f9d0726ad924e37ebc213bbe1179d0cca7a
KrenarLipo <39993811+KrenarLipo@users.noreply.github.com>
2021-12-15 14:17:07 +0100

    added outside views

---
+++ Code/outside_views/insideMenu.php
@@ -0,0 +1,7 @@
+<ul class="navbar-nav mr-auto">
+    <li class="nav-item ">
+        <a class="nav-link " href="mindex.php">Home <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item ">  <a class="nav-link" href="maddnew.php">Add New Account</a></li>
+    <li class="nav-item ">  <a class="nav-link" href="mfeedback.php">Feedback</a></li>
+</ul>
---
+++ Code/outside_views/userMenu.php
@@ -0,0 +1,11 @@
+<ul class="navbar-nav mr-auto">
+      <li class="nav-item ">
+        <a class="nav-link " href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item ">  <a class="nav-link" href="accounts.php">Accounts</a></li>
+      <li class="nav-item ">  <a class="nav-link" href="statements.php">Account Statements</a></li>
+      <li class="nav-item ">  <a class="nav-link" href="transfer.php">Funds Transfer</a></li>
+      <!-- <li class="nav-item ">  <a class="nav-link" href="profile.php">Profile</a></li> -->
+
+
+    </ul>
